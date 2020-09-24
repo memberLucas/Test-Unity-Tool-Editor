e433bf7aa75c2403864d3e2f9386926b7f82e726
luce <lukas-m.1@gmx.de>
2020-09-24 18:27:48 +0200

    erschaffe und zerstöre Würfel

--- Tool Editor/Assets/Editor/SimpleEditor.cs
+++ Tool Editor/Assets/Editor/SimpleEditor.cs
@@ -1,20 +1,64 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
-using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 
-public class SimpleEditor : Editor
+public class SimpleEditor : EditorWindow
 {
-    
-    void ShowWindow()
-    {
+    VisualElement root;
+    List<GameObject> _go = new List<GameObject>(); 
 
+    [MenuItem("Tool/Beispiel")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<SimpleEditor>();
+
+        window.titleContent =new GUIContent("Simple Editor");
+        window.minSize = new Vector2(150f, 50f);
     }
 
     private void OnEnable()
     {
-        
+        root = rootVisualElement;
+        AddButtonsToGui();   
     }
+
+    void AddButtonsToGui()
+    {
+        //VisualElement root = rootVisualElement;
+        var button = new Button() { text = "Create Cube" };
+
+        button.clicked += AddCube;
+        button.style.width = 150f;
+        button.style.height = 25f;
+
+        root.Add(button);
+
+
+        button = new Button() { text = "Destroy Cube" };
+
+        button.clicked += DestroyCube;
+        button.style.width = 150f;
+        button.style.height = 25f;
+
+        root.Add(button);
+    }
+
+    void AddCube()
+    {
+        var Go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Go.transform.position = new Vector3(5, 0, 0);        
+        _go.Add(Go);
+    }
+
+    void DestroyCube()
+    {
+        foreach (var item in _go)
+        {
+            DestroyImmediate(item);
+        }
+    }
+
 }
