5f67d426964a402e22799e65462fd0581b9d2d8c
luce <lukas-m.1@gmx.de>
2020-09-24 17:10:16 +0200

    Leeres Projekt

---
+++ Tool Editor/Assets/Editor/SimpleEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+
+public class SimpleEditor : Editor
+{
+    
+    void ShowWindow()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+}
